44663a6d4eaf6f950a3bc74a32ac558bfe535fae
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-06 17:10:26 +0530

    Registered aboutus app

--- Warehouse/urls.py
+++ Warehouse/urls.py
@@ -20,6 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^aboutus/', include('aboutus.urls')),
     url(r'^$', include('basic.urls')),
 ]
 
