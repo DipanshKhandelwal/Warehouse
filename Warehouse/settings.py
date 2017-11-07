e4161e29dca4e9104b94206847d195448ba642b6
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-08 02:42:37 +0530

    Added login and logout urls in settings.py

--- Warehouse/settings.py
+++ Warehouse/settings.py
@@ -118,6 +118,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = 'store'
+LOGOUT_REDIRECT_URL = 'home_page'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
