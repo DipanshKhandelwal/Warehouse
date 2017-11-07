05eb6285e02e67c5c4befbd8bf3cf00de31bd038
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-07 10:31:15 +0530

    Added model Item

--- basic/models.py
+++ basic/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Item(models.Model):
+    name = models.CharField(max_length=20)
+    max_storage_temperature = models.FloatField(default=2)
+    min_storage_temperature = models.FloatField(default=-10)
+    max_storage_time = models.IntegerField()
+    max_storage_available = models.IntegerField()
+    storage_filled = models.IntegerField()
+
+    def __str__(self):
+        return self.name
