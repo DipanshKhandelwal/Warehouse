8cb064755406095a356c4f6e2679f87177351582
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-08 02:57:51 +0530

    Added login and logout urls

--- Warehouse/urls.py
+++ Warehouse/urls.py
@@ -17,9 +17,12 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^aboutus/', include('aboutus.urls')),
     url(r'^store/', include('store.urls')),
     url(r'^sell/', include('sell.urls')),
