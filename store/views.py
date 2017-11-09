022123cb020da58923720995b19b74cb0a931b2c
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-09 23:59:34 +0530

    Added login required view to see the store page

--- store/views.py
+++ store/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from basic.models import Stock, Item
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url='/login')
 def store(request):
-    return render(request, 'store/store.html')
+    my_list = {}
+    for i in Item.objects.all():
+        total = 0
+        for s in Stock.objects.filter(item=i, master=request.user):
+            total += s.quantity
+            my_list.setdefault(i.name, total)
+
+        my_list2 = Stock.objects.filter(master=request.user)
+    return render(request, 'store/store.html', {'dict': my_list, 'dict2': my_list2})
