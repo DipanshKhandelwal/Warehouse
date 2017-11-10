ecd3b2178a636e27a2c383e55e2ccdc47678f80c
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-11 03:56:17 +0530

    Added view to display all the stock data pdfs available

--- sell/views.py
+++ sell/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import os
 
 
 # Create your views here.
 def sell(request):
-    return render(request, 'sell/sell.html')
+    my_list = []
+    for file in os.listdir("buy/static/pdf/"):
+        if file.endswith(".pdf"):
+            file = file[:len(file)-4]
+            my_list.append(file)
+    return render(request, 'sell/sell.html', {'go': my_list})
