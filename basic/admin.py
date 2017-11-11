00091ed0fbe68b03528491e92cb4be7145449179
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-11 12:31:29 +0530

    Registered model Farmer to admin

--- basic/admin.py
+++ basic/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Item, Stock
+from .models import Item, Stock, Farmer
 
 # Register your models here.
-my_models = [Item, Stock]
+my_models = [Item, Stock, Farmer]
 admin.site.register(my_models)
