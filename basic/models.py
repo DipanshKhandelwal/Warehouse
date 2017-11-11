262f718427c23771adc91b4627b757c830fac91f
Dipansh Khandelwal <deepanshrocks438@gmail.com>
2017-11-11 12:30:53 +0530

    Added new model Farmer

--- basic/models.py
+++ basic/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import RegexValidator
 
 
 # Create your models here.
@@ -22,3 +23,13 @@ class Stock(models.Model):
 
     def __str__(self):
         return self.item.name + '-' + str(self.quantity)
+
+
+class Farmer(models.Model):
+    user = models.OneToOneField(User)
+    address = models.TextField()
+    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',message="Phone number must be entered in the format: '+999999999'. Up to 15 digits allowed.")
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, blank=True)  # validators should be a list
+
+    def __str__(self):
+        return self.user.username + '-' + self.phone_number
